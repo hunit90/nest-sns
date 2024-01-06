cfac921a90931b04aa21c1fb3a9135a9f663220a
hunit <hunit612@naver.com>
2024-01-06 21:33:06 +0900

    feat: get post

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,8 @@
-import {Controller, Get} from '@nestjs/common';
+import {Controller, Get, NotFoundException, Param} from '@nestjs/common';
 import { PostsService } from './posts.service';
 
-interface Post {
+interface PostModel {
+  id: number;
   author: string;
   title: string;
   content: string;
@@ -9,18 +10,50 @@ interface Post {
   commentCount: number;
 }
 
+let posts : PostModel[] = [
+  {
+    id: 1,
+    author: 'newjeans_official',
+    title: '뉴진스 민지',
+    content: '메이크업 고치고 있는 민지',
+    likeCount: 1000,
+    commentCount: 9999,
+  },
+  {
+    id: 2,
+    author: 'newjeans_official',
+    title: '뉴진스 혜린',
+    content: '노래 연습하는 혜린',
+    likeCount: 1000,
+    commentCount: 9999,
+  },
+  {
+    id: 3,
+    author: 'blackpink_official',
+    title: '블랙핑크 제니',
+    content: '공연중인 제니',
+    likeCount: 1000,
+    commentCount: 9999,
+  },
+]
+
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @Get()
-  getPost(): Post {
-    return {
-      author: 'newjeans_official',
-      title: 'minji',
-      content: 'makeup change',
-      likeCount: 10000,
-      commentCount: 9999,
+  getPosts() {
+    return posts;
+  }
+
+  @Get(':id')
+  getPost(@Param('id') id: string) {
+    const post = posts.find((post) => post.id === +id)
+
+    if(!post) {
+      throw new NotFoundException()
     }
+
+    return post;
   }
 }
